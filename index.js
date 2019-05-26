dce6b4b04c70774fdaf09064522190306c80badd
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-05-26 11:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-function logShout(string) {
-  console.log(string.toUpperCase)
+function whisper(string) {
+  return string.toLowerCase()
 }
 
+function shout(string) {
+  return string.toUpperCase()
+}
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  string.toUpperCase() === string
+  return "YES INDEED!"
+  string.toLowerCase() === string
+  return "I can't hear you!"
+}
